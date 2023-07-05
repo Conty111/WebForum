d2be45132f12ec86441f139a29b73ec44928cd75
Pipi <mitj88@gmail.com>
2023-07-05 22:23:57 +0300

    Edit config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config(object):
     DEBUG = True
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'fpasnfok;lm23452903'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data.db'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database/data.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     ################
